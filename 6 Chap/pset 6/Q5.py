294677c4ce2c93ac2f5cc07725dbb05ebceefa93
pathan-kaif-khan <110097550+pathan-kaif-khan@users.noreply.github.com>
2025-01-28 21:56:05 +0530

    Add files via upload

---
+++ 6 Chap/pset 6/Q5.py
@@ -0,0 +1,8 @@
+l = ["kaihan", "rahim","kabir"]
+
+name= input("enter your name:")
+
+if(name in l):
+    print("your name is in the list")
+else:
+    print("your name is not available in the list")

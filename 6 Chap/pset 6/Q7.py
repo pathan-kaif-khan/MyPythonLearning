84d925ecc1400d67457c4fb7dd9fa5462d6036f7
pathan-kaif-khan <110097550+pathan-kaif-khan@users.noreply.github.com>
2025-01-28 21:56:58 +0530

    Add files via upload

---
+++ 6 Chap/pset 6/Q7.py
@@ -0,0 +1,6 @@
+post = input()
+
+if("harry" in post.lower()):  #it converts value inside the post to lower case
+    print("yes it talks about kaihan ")
+else:
+    print("no it do not talks about kaihan")

0fcd7bf4643e63bd0b65e413d195becdc8033ba7
pathan-kaif-khan <110097550+pathan-kaif-khan@users.noreply.github.com>
2025-01-29 23:27:04 +0530

    Add files via upload

---
+++ 7 Chap/pset 7/Q4.py
@@ -0,0 +1,9 @@
+#given number is prime or not
+n = int(input("enter num:"))
+
+for i in range(2,n):
+    if(n%i)==0:
+        print("number is not prime")
+        break
+else:
+    print("number is prime")

8eef39496f666140f9a753c204727657aa32f52c
pathan-kaif-khan <110097550+pathan-kaif-khan@users.noreply.github.com>
2025-01-30 00:21:14 +0530

    Add files via upload

---
+++ 7 Chap/pset 7/Q6.py
@@ -0,0 +1,7 @@
+#calculate factorial of given number
+# 5! = 1x2x3x4x5
+n = int(input("enter num:"))
+product = 1
+for i in range(1,n+1):
+    product=product*i
+print(f"The factorial of {n} is {product}")

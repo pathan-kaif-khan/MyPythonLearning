673eb5f6b04fd1c6aaa29ab2bf0fbc55dd816dc4
pathan-kaif-khan <110097550+pathan-kaif-khan@users.noreply.github.com>
2026-07-01 21:34:29 +0530

    Implement solutions for HackerRank Python
    Added solutions for HackerRank Python challenges including conditional checks, loops, and a leap year function.

--- Extra/revision.py
+++ Extra/revision.py
@@ -1,1 +1,49 @@
+# HACKER RANK PYTHON #
+# Q1
+if __name__ == '__main__':
+    n = int(input().strip())
 
+    if n % 2 != 0:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+  # Q2
+if __name__ == '__main__':
+    n = int(input())
+
+    for i in range(n):
+        print(i ** 2)
+
+# Q3
+if __name__ == '__main__':
+    n = int(input())
+
+    for i in range(n):
+        print(i ** 2)
+
+# Q4
+def is_leap(year):
+    leap = False
+
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
+
+    return leap
+
+year = int(input())
+print(is_leap(year))
+
+# Q5
+n = int(input())
+
+for i in range(1, n + 1):
+    print(i, end="")
+  

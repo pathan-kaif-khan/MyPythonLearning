409204d5f0bd5b23decf1ffd37b217406fe59588
pathan-kaif-khan <110097550+pathan-kaif-khan@users.noreply.github.com>
2026-07-04 20:52:56 +0530

    Update hacker rank 2.py

--- Extra/hacker rank 2.py
+++ Extra/hacker rank 2.py
@@ -40,3 +40,19 @@ if __name__ == '__main__':
     runner_up_students.sort() #to sort names in alphabetically
     for name in runner_up_students:
       print(name) 
+
+### Finding percentage ###
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    query_scores = student_marks[query_name]
+    
+    average_scores = sum(query_scores) / len(query_scores)
+    
+    print(f"{average_scores:.2f}")
